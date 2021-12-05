83dde53548c71334467947a45c095c5e75db8566
Vladyslav Starichenko <vladyslav.starichenko@nure.ua>
2021-12-05 22:25:10 +0200

    Mission page.

--- src/screens/Mission/mission.js
+++ src/screens/Mission/mission.js
@@ -1,10 +1,23 @@
+// Assets
+import forestPicture from '../../assets/image/forestPicture.jpeg';
+
 // Styles
 import './styles.scss';
 
 function Mission() {
   return (
-    <div>
-      <h1>Mission</h1>
+    <div className="mission-outer-container">
+      <div className="mission-description">
+        <h1 className="mission-header">OUR MISSION</h1>
+        <p className="mission-text">
+          ...is to revolutionize recycling chain
+          by creating a systematic solution that
+          fully automizes waste management, which
+          will lead to sustainable, clean World
+          and better future for generations.
+        </p>
+      </div>
+      <img src={forestPicture} alt="Forest" className="forest-background-image" />
     </div>
   );
 }
